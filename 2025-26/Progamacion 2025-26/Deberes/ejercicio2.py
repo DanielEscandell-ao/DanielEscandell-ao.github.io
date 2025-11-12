2772c57175a472ae2956285133b5553b6d6b82c5
DanielEscandell-ao <sm24.descandell@iessantamariadeivissa.com>
2025-11-12 11:52:54 +0100

    v8.3.1

---
+++ 2025-26/Progamacion 2025-26/Deberes/ejercicio2.py
@@ -0,0 +1,8 @@
+# Para el próximo día me váis a investigar qué hacen los siguientes métodos e las listas.
+list.append() # Sirve para añadir un valor nuevo al final de la lista.
+list.clear() # Sirve para eliminar todos los valores de una lista.
+list.count() # Sirve para contar las veces que aparece un valor en la lista.
+list.extend() # Este es como el append pero para añadir varios valores a la vez.
+list.insert() # Sirve para añadir un valor nuevo en una posicion especifica dentro de la lista.
+list.remove() # Sirve para quitar la primera aparicion de un valor en especifico.
+list.reverse() # Sirve para darle la vuelta a la lista.
